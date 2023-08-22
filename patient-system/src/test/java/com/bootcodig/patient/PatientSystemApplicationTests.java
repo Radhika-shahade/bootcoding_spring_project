a3c3bb90fd46164a29c6ff19a4cd6a3c319400a3
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-22 22:13:19 +0530

    Patient service

---
+++ patient-system/src/test/java/com/bootcodig/patient/PatientSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcodig.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
