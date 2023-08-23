7105983a24426afdff3485e67085015b3e3bf3c1
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-23 18:16:35 +0530

    coupon Application

---
+++ coupon-system/src/main/java/com/bootcodig/couponsystem/Util/Discount.java
@@ -0,0 +1,16 @@
+package com.bootcodig.couponsystem.Util;
+
+import java.util.Random;
+
+public class Discount {
+    public static double discount()
+    {
+        Random random= new Random();
+      double max=85.40;
+      double min=15;
+      return random.nextDouble(max-min);
+      }
+
+    }
+
+
