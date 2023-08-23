66c0abfb02fac4f19c9acd0f486157f56a19cbf6
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-23 18:14:12 +0530

    coupon Application

---
+++ coupon-system/src/main/java/com/bootcodig/couponsystem/Util/CouponDescription.java
@@ -0,0 +1,18 @@
+package com.bootcodig.couponsystem.Util;
+
+import java.util.Random;
+
+public class CouponDescription {
+
+    public static String description()
+    {
+        Random random= new Random();
+        String s[] ={"Get a whopping 50% off your purchase for today only!","Enjoy free shipping on all orders with this special coupon",
+        "Buy one item, get one free – don't miss this deal!","Exclusive offer for our valued members – save big!",
+        "Flash sale happening this weekend – grab your deals!","Welcome new customers with a discount on your first order"};
+        int index= random.nextInt(s.length);
+        return s[index];
+
+    }
+
+}
