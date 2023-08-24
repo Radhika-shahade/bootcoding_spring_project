2e119717e38fdb4cbdda7d906a281ae6d78db8a8
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-24 09:05:09 +0530

    coupon Application

---
+++ coupon-system/src/main/java/com/bootcodig/couponsystem/Util/ValidDate.java
@@ -0,0 +1,27 @@
+package com.bootcodig.couponsystem.Util;
+
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class ValidDate {
+
+    public static Date randomDate() {
+        long aDay = TimeUnit.DAYS.toMillis(1); // 1 day
+        long now = new Date().getTime(); // current time
+        Date yesterday = new Date(now - aDay);
+        Date aYearAgo = new Date(now - aDay * 365);
+        return between(aYearAgo, yesterday);
+    }
+
+    private static Date between(Date startInclusive, Date endExclusive) {
+        long startMillis = startInclusive.getTime();
+        long endMillis = endExclusive.getTime();
+        long randomMillisSinceEpoch = ThreadLocalRandom
+                .current()
+                .nextLong(startMillis, endMillis);
+
+        return new Date(randomMillisSinceEpoch);
+    }
+
+}
