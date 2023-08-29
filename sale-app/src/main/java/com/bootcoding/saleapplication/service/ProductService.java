c4dd29f426428c26b15afce8b900b02b8b6df3b6
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-29 22:08:36 +0530

    sales Application

---
+++ sale-app/src/main/java/com/bootcoding/saleapplication/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.bootcoding.saleapplication.service;
+
+import com.bootcoding.saleapplication.model.Customer;
+import com.bootcoding.saleapplication.model.Product;
+import com.bootcoding.saleapplication.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+}
