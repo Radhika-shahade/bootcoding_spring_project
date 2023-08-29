a7f737c039d6235e5a0d7e1bc41dd2c33a5ad205
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-29 22:08:02 +0530

    sales Application

---
+++ sale-app/src/main/java/com/bootcoding/saleapplication/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.bootcoding.saleapplication.controller;
+
+import com.bootcoding.saleapplication.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+}
