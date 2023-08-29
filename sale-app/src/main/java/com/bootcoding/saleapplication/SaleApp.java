3650f7f59dabf53940db449fe6666111cbb23252
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-29 22:08:50 +0530

    sales Application

---
+++ sale-app/src/main/java/com/bootcoding/saleapplication/SaleApp.java
@@ -0,0 +1,13 @@
+package com.bootcoding.saleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleApp.class, args);
+	}
+
+}
