4578efed4574166792d7cda8a0f57b0de02cd2a0
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-31 15:18:09 +0530

    used response entity

--- sale-app/src/main/java/com/bootcoding/saleapplication/controller/ProductController.java
+++ sale-app/src/main/java/com/bootcoding/saleapplication/controller/ProductController.java
@@ -1,9 +1,14 @@
 package com.bootcoding.saleapplication.controller;
 
+import com.bootcoding.saleapplication.model.Product;
 import com.bootcoding.saleapplication.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -11,5 +16,11 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    @PostMapping("/products/{value}")
+    public List<Product> insertData(@PathVariable("value") int value)
+    {
+        return  productService.save(value);
+    }
+
 
 }
