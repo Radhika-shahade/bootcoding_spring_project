45e0f9784efeda1a8f10c4bbf2d87f2233b4f574
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-31 15:17:07 +0530

    used response entity

--- sale-app/src/main/java/com/bootcoding/saleapplication/model/Customer.java
+++ sale-app/src/main/java/com/bootcoding/saleapplication/model/Customer.java
@@ -1,20 +1,23 @@
 package com.bootcoding.saleapplication.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     @Id
     @GeneratedValue
     private int id;
     private String name;
     @OneToMany(targetEntity = Product.class, cascade = CascadeType.ALL)
-    @JoinColumn(name = "productId", referencedColumnName = "id")
     private List<Product>products;
 }
